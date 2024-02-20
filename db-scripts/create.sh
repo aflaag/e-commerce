12e459b47605b2b9704e08f4e1ed974b14a91b5a
aflaag <alessio.bandiera02@gmail.com>
2024-02-20 17:40:39 +0100

    privilges to test extension

--- db-scripts/create.sh
+++ db-scripts/create.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
 
-# This defines parameters
-# parameters.sql 
-
-# This creates db and user
-# create-db-user.sql
-
-# This creates db schema (tables)
-# schema.sql
-
-# Grant priviliges to user
-# grant.sql
-
-
-# This does all
-# sudo -u postgres psql postgres -f parameters.sql -f create-db-user.sql  -f schema.sql  -f grant.sql
-sudo -u postgres psql postgres -f parameters.sql -f setup.sql  -f schema.sql
-
+sudo -u postgres psql postgres -f parameters.sql -f setup.sql  -f schema.sql -f grant.sql
--- db-scripts/grant.sql
+++ db-scripts/grant.sql
@@ -2,14 +2,14 @@
 \c :dbname postgres
 
 -- user already exists
-GRANT ALL PRIVILEGES ON DATABASE :dbname to :username ;
+GRANT ALL PRIVILEGES ON DATABASE :dbname to :admin ;
 
 
-ALTER TABLE TimeVar OWNER TO :username ;
-ALTER TABLE LogTable OWNER TO :username ;
+-- ALTER TABLE TimeVar OWNER TO :admin ;
+-- ALTER TABLE LogTable OWNER TO :admin ;
 
--- grant all privileges on all tables in schema public to :username ;
--- grant all privileges on all sequences in schema public to :username ;
+-- grant all privileges on all tables in schema public to :admin ;
+-- grant all privileges on all sequences in schema public to :admin ;
 
-GRANT ALL ON SCHEMA public TO :username ;
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO :username ;
+GRANT ALL ON SCHEMA public TO :admin ;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO :admin ;
