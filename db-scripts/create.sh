df9df5697e2b21f5c7e4d08ee84e19d4e831574e
aflaag <alessio.bandiera02@gmail.com>
2024-02-22 16:59:10 +0100

    super important changes

--- db-scripts/create.sh
+++ db-scripts/create.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 #sudo -u postgres psql postgres -f parameters.sql -f setup.sql -f schema.sql -f grant.sql -f triggers.sql
-sudo -u postgres psql postgres -f parameters.sql -f setup.sql -f schema.sql -f grant.sql -f triggers.sql -f base_data.sql -f tests.sql
+sudo -u postgres psql postgres -f parameters.sql -f setup.sql -f schemas.sql -f permissions.sql -f triggers.sql -f fill.sql -f tests.sql
 #sudo -u postgres psql postgres -f parameters.sql -f triggers.sql
