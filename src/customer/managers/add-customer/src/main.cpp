7a50c51a1fc5b9c0a76e050ded0243a45f2f2989
I0nut02 <ionu.radu02@gmail.com>
2024-02-24 11:01:37 +0100

    add formal structure

--- src/customer/managers/add-customer/src/main.cpp
+++ src/customer/managers/add-customer/src/main.cpp
@@ -1,61 +1,92 @@
 #include "main.h"
 
-#define READ_STREAM "read_stream"
+#define READ_STREAM "handler-add-customer"
+#define WRITE_STREAM "add-customer-handler"
 
 int main() {
     redisContext *c2r;
     redisReply *reply;
+
+    PGresult *res;
+    
+    char query[1000];
+
+    char name[100];
+    char email[100];
+    char surname[100];
+    char phone_number[100];
+    char response[100];
+    int k, i, h;
     char key[100];
     char value[100];
-    int numstreams;
-    int k, i, h;
-    char fval[100];
 
+
+    Con2DB db("localhost", "5432", "customer", "customer", "ecommerce");
     c2r = redisConnect("localhost", 6379);
 
     // delete stream if exists
     reply = RedisCommand(c2r, "DEL %s", READ_STREAM);
     assertReply(c2r, reply);
     dumpReply(reply, 0);
+    reply = RedisCommand(c2r, "DEL %s", WRITE_STREAM);
+    assertReply(c2r, reply);
+    dumpReply(reply, 0);
 
     // initialize stream
     initStreams(c2r, READ_STREAM);
+    initStreams(c2r, WRITE_STREAM);
     
     add_to_stream();
 
-    reply = RedisCommand(c2r, "XREADGROUP GROUP diameter Alice BLOCK 0 COUNT 4 STREAMS %s >", READ_STREAM);
+    while(1) {
+        reply = RedisCommand(c2r, "XREADGROUP GROUP diameter Alice BLOCK 0 COUNT 4 STREAMS %s >", READ_STREAM);
 
-    assertReply(c2r, reply);
+        assertReply(c2r, reply);
 
-    for (k=0; k < ReadNumStreams(reply); k++) {
-        for (i=0; i < ReadStreamNumMsg(reply, k); i++) {
-	        for (h = 0; h < ReadStreamMsgNumVal(reply, k, i); h +=  2) {
-		        ReadStreamMsgVal(reply, k, i, h, key);
-                ReadStreamMsgVal(reply, k, i, h, value);
-                printf("key = %s, value = %s\n", key, value);
-		    }	      	      
+        for (k=0; k < ReadNumStreams(reply); k++) {
+            for (i=0; i < ReadStreamNumMsg(reply, k); i++) {
+                for (h = 0; h < ReadStreamMsgNumVal(reply, k, i); h +=  2) {
+                    ReadStreamMsgVal(reply, k, i, h, key);
+                    ReadStreamMsgVal(reply, k, i, h, value);
+                    
+                    if (key == "name") {
+                        sprintf(name, "%s", value);
+                    } else if (key == "email") {
+                        sprintf(email, "%s", value);
+                    } else if (key == "surname") {
+                        sprintf(surname, "%s", value);
+                    } else if (key == "phone_number") {
+                        sprintf(phone_number, "%s", value);
+                    } else {
+                        printf("%s\n", key);
+                        return 1;
+                    }
+                }	      	      
+            }
         }
+
+        freeReplyObject(reply);
+
+        sprintf(query, "INSERT INTO Customer (email, name, surname , phone_number) VALUES (\'%s\', \'%s\', \'%s\', \'%s\')", 
+                        email, name, surname, phone_number);
+
+        res = db.RunQuery(query, false);
+
+        if (PQresultStatus(res) != PGRES_COMMAND_OK && PQresultStatus(res) != PGRES_TUPLES_OK) {
+            // gestione disconnessione db o per il meme ci connettiamo sempre
+            sprintf(response, "BAD REQUEST");
+        } else {
+            sprintf(response, "SUCCESSFUL REQUEST");
+        }
+
+        reply = RedisCommand(c2r, "XADD %s * response %s", WRITE_STREAM);
+        assertReplyType(c2r, reply, REDIS_REPLY_STRING);
+        freeReplyObject(reply);
+
+        // continuiamo con tuple se dobbiamo ritornare qualcosa
     }
 
-    freeReplyObject(reply);
-
-    // Con2DB db("localhost", "5432", "customer", "customer", "ecommerce");
-
-    // PGresult *res;
-    
-    // char query[1000];
-
-    // sprintf(query, "INSERT INTO Customer (email, name, surname , phone_number) VALUES (\'c@gmail.com\', \'a\', \'boh\', \'+12345678\')");
-
-    // res = db.RunQuery(query, false);
-
-    // if (PQresultStatus(res) != PGRES_COMMAND_OK && PQresultStatus(res) != PGRES_TUPLES_OK) {
-    //     db.finish();
-
-    //     return -1;
-    // }
-
-    // db.finish();
+    db.finish();
 
     return 0;
 }
--- src/customer/managers/add-customer/src/test_stream.cpp
+++ src/customer/managers/add-customer/src/test_stream.cpp
@@ -1,6 +1,6 @@
 #include "test_stream.h"
 
-#define READ_STREAM "read_stream"
+#define WRITE_STREAM2 "handler-add-customer"
 
 int add_to_stream() {
     redisContext *c2r;
@@ -9,14 +9,14 @@ int add_to_stream() {
     c2r = redisConnect("localhost", 6379);
 
     // delete stream if exists
-    reply = RedisCommand(c2r, "DEL %s", READ_STREAM);
+    reply = RedisCommand(c2r, "DEL %s", WRITE_STREAM2);
     assertReply(c2r, reply);
     dumpReply(reply, 0);
 
     // initialize stream
-    initStreams(c2r, READ_STREAM);
+    initStreams(c2r, WRITE_STREAM2);
 
-    reply = RedisCommand(c2r, "XADD %s * cazzo palle cazzo palle cazzo palle cazzo palle", READ_STREAM);
+    reply = RedisCommand(c2r, "XADD %s * name example surname example email example@example.com phone_sumber +34567453", WRITE_STREAM2);
     assertReplyType(c2r, reply, REDIS_REPLY_STRING);
     freeReplyObject(reply);
 
