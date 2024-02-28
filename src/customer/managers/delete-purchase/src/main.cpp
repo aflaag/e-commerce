b7460dc36c7ee7151b756c2b9330320421d2fbd6
saralazza <slazzds@gmail.com>
2024-02-28 14:32:29 +0100

    implement delete order functionalities

---
+++ src/customer/managers/delete-purchase/src/main.cpp
@@ -0,0 +1,84 @@
+#include "main.h"
+
+int main() {
+    redisContext *c2r;
+    redisReply *reply;
+
+    PGresult *query_res;
+
+    char query[1000], response[100], msg_id[30], first_key[30], client_id[30], second_key[30], purchase[100];
+
+    char * cancel_instant;
+
+    Con2DB db(POSTGRESQL_SERVER, POSTGRESQL_PORT, POSTGRESQL_USER, POSTGRESQL_PSW, POSTGRESQL_DBNAME);
+    c2r = redisConnect(REDIS_SERVER, REDIS_PORT);
+
+    // delete stream if exists
+    reply = RedisCommand(c2r, "DEL %s", READ_STREAM);
+    assertReply(c2r, reply);
+    dumpReply(reply, 0);
+
+    reply = RedisCommand(c2r, "DEL %s", WRITE_STREAM);
+    assertReply(c2r, reply);
+    dumpReply(reply, 0);
+
+    // initialize stream
+    initStreams(c2r, READ_STREAM);
+    initStreams(c2r, WRITE_STREAM);
+    
+    add_to_stream(); // DA TOGLIERE
+
+    while(1) {
+
+        reply = RedisCommand(c2r, "XREADGROUP GROUP main customer BLOCK 0 COUNT 1 STREAMS %s >", READ_STREAM);
+
+        assertReply(c2r, reply);
+
+        if (ReadNumStreams(reply) == 0) {
+            continue;
+        } 
+
+        // Only one stream --> stream_num = 0
+        // Only one message in stream --> msg_num = 0
+        ReadStreamNumMsgID(reply, 0, 0, msg_id);
+
+        // Check if the first key/value pair is the client_id
+        ReadStreamMsgVal(reply, 0, 0, 0, first_key);    // Index of first field of msg = 0
+        ReadStreamMsgVal(reply, 0, 0, 1, client_id);    // Index of second field of msg = 1
+
+        if(strcmp(first_key, "client_id")){
+            send_response_status(c2r, WRITE_STREAM, client_id, "INVALID#CLIENT#STREAM", msg_id);
+            continue;
+        }
+
+        // Take id of purchase
+        ReadStreamMsgVal(reply, 0, 0, 2, second_key);    // Index of first field of msg = 0
+        ReadStreamMsgVal(reply, 0, 0, 3, purchase);    // Index of second field of msg = 1
+
+        if(strcmp(second_key, "purchase")){
+            send_response_status(c2r, WRITE_STREAM, client_id, "INVALID#REQUEST#STREAM", msg_id);
+            continue;
+        }
+
+        // Take cancel instant
+        auto current_time = std::chrono::system_clock::now();
+        std::time_t current_time_t = std::chrono::system_clock::to_time_t(current_time);
+        cancel_instant = std::ctime(&current_time_t);
+        
+
+        sprintf(query, "UPDATE Purchase SET cancel_instant = \'%s\' WHERE id = %s;", cancel_instant, purchase);
+
+        query_res = db.RunQuery(query, false);
+
+        if (PQresultStatus(query_res) != PGRES_COMMAND_OK && PQresultStatus(query_res) != PGRES_TUPLES_OK) {
+            send_response_status(c2r, WRITE_STREAM, client_id, "DB#ERROR", msg_id);
+            continue;
+        }
+
+        send_response_status(c2r, WRITE_STREAM, client_id, "UPDATE#SUCCESS", msg_id);
+    }
+
+    db.finish();
+
+    return 0;
+}
---
+++ src/customer/managers/delete-purchase/src/main.h
@@ -0,0 +1,43 @@
+#ifndef main_h
+#define main_h
+
+/* Library imports */
+// TODO: da filtrare
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <string>
+#include <cassert>
+#include <cerrno>
+
+/* Local imports */
+
+#include "../../../../lib/con2db/pgsql.h"
+#include "../../../../lib/con2redis/src/con2redis.h"
+#include "test_stream.h"
+#include "../../../../classes/src/purchase.h"
+#include "../../../../classes/src/ordered_product.h"
+#include "../../../../utils/src/utils.h"
+
+/* Constants */
+
+#define READ_STREAM "delete-purchase-in"
+#define WRITE_STREAM "delete-purchase-out"
+
+#define REDIS_SERVER "localhost"
+#define REDIS_PORT 6379
+
+#define POSTGRESQL_SERVER "localhost"
+#define POSTGRESQL_PORT "5432"
+#define POSTGRESQL_USER "customer"
+#define POSTGRESQL_PSW "customer"
+#define POSTGRESQL_DBNAME "ecommerce"
+
+using namespace std;
+
+#endif
