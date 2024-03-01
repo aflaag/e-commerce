d53e299b54cc7e1071b15881243c114fb53b8b67
I0nut02 <ionu.radu02@gmail.com>
2024-03-01 18:09:43 +0100

    update client

--- socket/src2/client.cpp
+++ socket/src2/client.cpp
@@ -1,4 +1,4 @@
-#include <cstring> 
+UpdateRefund#include <cstring> 
 #include <iostream> 
 #include <netinet/in.h> 
 #include <sys/socket.h> 
@@ -8,7 +8,7 @@
   
 using namespace std; 
 
-#define SERVER_PORT  12345
+#define SERVER_PORT  8080
 
 
 int micro_sleep(long usec)
@@ -36,25 +36,43 @@ int micro_sleep(long usec)
 int main() 
 { 
     // creating socket 
-    int clientSocket = socket(AF_INET6, SOCK_STREAM, 0); 
-  struct sockaddr_in6 addr;
+    int clientSocket = socket(AF_INET, SOCK_STREAM, 0); 
+  int rc;
+  int len;
+  char buffer[1000];
+
     // specifying address 
-    memset(&addr, 0, sizeof(addr));
-   addr.sin6_family      = AF_INET6;
-   memcpy(&addr.sin6_addr, &in6addr_any, sizeof(in6addr_any));
-   addr.sin6_port        = htons(SERVER_PORT);
+    sockaddr_in addr; 
+    addr.sin_family = AF_INET; 
+    addr.sin_port = htons(8080); 
+    addr.sin_addr.s_addr = INADDR_ANY; 
   
     // sending connection request 
 
     connect(clientSocket, (struct sockaddr*)&addr, 
             sizeof(addr)); 
-    int i = 0;
-    while(i < 10){
-    connect(clientSocket, (struct sockaddr*)&addr, 
-            sizeof(addr));
-    cout << i << endl;
-    i ++;
-    }
+
+    const char* message = "Hello, server!"; 
+    send(clientSocket, message, strlen(message), 0);
+    printf("banan\n");
+    rc = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (rc < 0)
+        {
+            if (errno != EWOULDBLOCK)
+            {
+            perror("  recv() failed");
+            }
+        }
+
+        if (rc == 0)
+        {
+            printf("  Connection closed\n");
+        }
+
+        len = rc;
+        printf("  %d bytes received\n", len);
+    
+    
     // closing socket 
     close(clientSocket); 
   
