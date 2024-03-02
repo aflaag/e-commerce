aab19e52951d718ff71f29d822ff18759ab74d47
aflaag <alessio.bandiera02@gmail.com>
2024-03-02 12:11:41 +0100

    non ho fatto niente

---
+++ src/customer/managers/view-order/src/test_stream.h
@@ -0,0 +1,26 @@
+#ifndef test_stream_h
+#define test_stream_h
+
+// TODO: da filtrare
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <stddef.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/times.h>
+#include <cassert>
+#include <cerrno>
+
+#include "../../../../lib/con2redis/src/con2redis.h"
+#include "../../../../utils/src/utils.h"
+
+using namespace std;
+
+int add_to_stream();
+int read_from_stream();
+
+#endif
