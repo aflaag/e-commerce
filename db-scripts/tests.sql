3cd1e75ec9c9172b770f8acc0e9de4817b72ab21
I0nut02 <ionu.radu02@gmail.com>
2024-03-02 21:52:35 +0100

    add data for refunded orders

--- db-scripts/tests.sql
+++ db-scripts/tests.sql
@@ -23,10 +23,20 @@ INSERT INTO OrderedProducts (product, purchase, quantity)
 VALUES ('P2', 2, 20);
 
 INSERT INTO AssignedDelivery 
-VALUES('bho2','2024-02-22 10:30:00','2024-02-22 10:30:00', '2024-02-22 10:30:00','Delivered',null, 'courier1',1);
+VALUES('bho2','2024-02-22 10:30:00','2024-02-22 10:30:00', '2024-02-22 10:30:00','Delivered',null, 'courier1',2);
 
 INSERT INTO Restock (quantity, supplier, product)
 VALUES(100, 'Supplier1', 'P1');
 
 INSERT INTO RefundRequest (request_instant, refund_state, assigned_delivery)
 VALUES ('2024-02-22 10:30:00', 'Requested', 'bho2');
+
+INSERT INTO RefundedProduct (refund_request, product, quantity)
+VALUES (1, 'P1', 10);
+
+INSERT INTO RefundedProduct (refund_request, product, quantity)
+VALUES (1, 'P2', 10);
+
+SELECT rp.product AS product, rp.quantity AS quantity, rr.refund_state AS state,rr.request_instant AS request_instant, rr.refund_assignment_instant AS assigned, rr.refund_start AS refund_start, rr.refund_end AS refund_end, rr.courier AS courier, ad.purchase AS purchase 
+FROM RefundRequest AS rr, RefundedProduct AS rp, AssignedDelivery AS ad 
+WHERE rr.id = '1' AND rr.id = rp.refund_request AND ad.delivery_code = rr.assigned_delivery;
